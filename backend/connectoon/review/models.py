f5bf103468d162fbfe0208c865fe877053a8d0fb
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-10-30 14:48:47 +0900

    added review models

--- backend/connectoon/review/models.py
+++ backend/connectoon/review/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
+class Review(models.Model):
+  work_id = models.ForeignKey(, on_delete=models.CASCADE)
+  author_id = models.ForeignKey(, on_delete=models.CASCADE)
+  score = models.FloatField()           #decimal?
+  likes = models.IntegerField(default=0)
+  title = models.CharField(max_length=200)  #length설정
+  content = models.CharField(max_length=1000) #length
+  updated_at = models.DateTimeField()
 # Create your models here.
