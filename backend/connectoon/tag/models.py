291892b43c6fa330c409b304cf94ca3aeb52d321
hursuhyun <38004175+hursuhyun@users.noreply.github.com>
2021-10-30 16:21:11 +0900

    Db structure (#12)
    * added tag models
    
    * added review models
    
    * added work models
    
    * added artist models
    
    * updated review models
    
    * updated review content to textfield
    
    Co-authored-by: hursuhyun <hursuhyun@csap.snu.ac.kr>

--- backend/connectoon/tag/models.py
+++ backend/connectoon/tag/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models.base import Model
+
+class Tag(models.Model):
+  name = models.CharField(max_length=30)
 
 # Create your models here.
--- backend/connectoon/work/models.py
+++ backend/connectoon/work/models.py
@@ -1,3 +1,22 @@
+from django.core import validators
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from backend.connectoon.artist.models import Artist
 
-# Create your models here.
+from backend.connectoon.tag.models import Tag
+
+
+class Work(models.Model):
+  title = models.CharField(max_length=200)  #length설정
+  thumbnail_picture = models.URLField()     #picture link 만 있으면 되려나.?
+  description = models.CharField(max_length=1000)
+  year = models.IntegerField(validators =[ MinValueValidator(1900), MaxValueValidator(2100) ])
+  link = models.URLField()
+  completion = models.BooleanField()
+  score_sum = models.FloatField(default=0)
+  review_num = models.IntegerField(default=0)
+  
+  PLATFORM_CHOICES = models.TextChoices('Naver', 'Kakao', 'Lezhin') 
+  platform_id = models.IntegerField(max_length=1, choices= PLATFORM_CHOICES)
+  tags = models.ManyToManyField(Tag, related_name="works") # 중간 table이 따로 필요없을 것 같은데 맞나요?
+  artists = models.ManyToManyField(Artist, related_name="works")
