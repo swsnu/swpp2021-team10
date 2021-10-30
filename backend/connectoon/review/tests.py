d4fc76b7f9faeb1596af5f655255e431c3704904
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-10-30 17:21:14 +0900

    artist finished

--- backend/connectoon/review/tests.py
+++ backend/connectoon/review/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
+
+from work.models import Work
 from .models import Review
+
 
 class ReviewTestCase(TestCase):
   def setUp(self):
-    Review.objects.create(work_id=1, score = 0.0, title = "DUM", content="DUM_CONTENT", updated_at ="")
+    Work.objects.create(title = 'DummyTitle', year=2019, description="HI", link="https://www.naver.com/",
+    completion=True, platform_id=1,
+    )
+    Review.objects.create(Work.objects.first, score = 0.0, title = "DUM", content="DUM_CONTENT", updated_at ="")
   
   def test_review_count(self):
     self.assertEqual(Review.objects.all().count(), 1)
