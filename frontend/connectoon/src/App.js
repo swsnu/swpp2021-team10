2725cdfcbc3e492c3e6a399ce80a3ccee0851bc7
kskim0119 <dooly9931@snu.ac.kr>
2021-10-30 14:28:48 +0900

    implement and design navbar

--- frontend/connectoon/src/App.js
+++ frontend/connectoon/src/App.js
@@ -7,14 +7,20 @@ import {
 import { Provider } from 'react-redux';
 import store from './store/store';
 import Main from './Containers/Main/Main';
+import NavBar from './Components/NavBar/NavBar';
+import Recommendation from './Containers/Recommendation/Recommendation';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <div className="App">
+          <NavBar className="nav-bar" />
+          <div className="nav-bar-empty-space" />
           <Switch>
             <Route path="/main" exact component={Main} />
+            <Route path="/recommendation" exact component={Recommendation} />
+            <Redirect from="/" to="/main" />
           </Switch>
         </div>
       </BrowserRouter>
