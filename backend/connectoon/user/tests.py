e3543c0ea6eaf94dacaacd893b2f7c26a9e25f7b
veldic <rsual@snu.ac.kr>
2021-11-05 18:12:43 +0900

    Add Custom User

--- backend/connectoon/user/tests.py
+++ backend/connectoon/user/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase, Client
 
 class UserTestCase(TestCase):
@@ -30,3 +31,22 @@ class UserTestCase(TestCase):
     response = client.get('/users/me/reviews/')
 
     self.assertEqual(response.status_code, 501)
+
+  class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+      User = get_user_model()
+      user = User.objects.create_user(email='veldic@user.com', password='qwe123', username='veldic')
+      self.assertEqual(user.email, 'veldic@user.com')
+      self.assertEqual(user.username, 'veldic')
+      self.assertTrue(user.is_active)
+      self.assertFalse(user.is_superuser)
+
+      with self.assertRaises(TypeError):
+        User.objects.create_user()
+      with self.assertRaises(TypeError):
+        User.objects.create_user(email='')
+      with self.assertRaises(TypeError):
+        User.objects.create_user(email='', password="qwe123")
+      with self.assertRaises(ValueError):
+        User.objects.create_user(email='', password="foo", username='veldic')
