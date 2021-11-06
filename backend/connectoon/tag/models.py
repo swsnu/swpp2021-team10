641563900e7f05f1a28489149dd2c2613e1cad5b
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-11-07 00:51:59 +0900

    changed indent to 4

--- backend/connectoon/tag/models.py
+++ backend/connectoon/tag/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.db.models.base import Model
 
 class Tag(models.Model):
-  name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
 # Create your models here.
