77815206396ea63ad025e76b4d4ba5e62ab9329d
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-11-06 23:48:56 +0900

    modified related_name

--- backend/connectoon/review/models.py
+++ backend/connectoon/review/models.py
@@ -6,8 +6,8 @@ from work.models import Work
 from django.contrib.auth import get_user_model
 
 class Review(models.Model):
-  work = models.ForeignKey(Work, on_delete=models.CASCADE, related_name="work")
-  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="author")
+  work = models.ForeignKey(Work, on_delete=models.CASCADE, related_name="comment")
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="comment")
   score = models.FloatField()        #decimal?
   likes = models.IntegerField(default=0)
   title = models.CharField(max_length=200)
