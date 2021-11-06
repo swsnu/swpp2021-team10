332809cc210f9cddc6975f94f6a9cdd18e19b091
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-11-07 03:56:55 +0900

    work id, review done except for login

--- backend/connectoon/work/tests.py
+++ backend/connectoon/work/tests.py
@@ -1,11 +1,30 @@
 from django.test import TestCase, Client
+from django.forms.models import model_to_dict
+from django.core import serializers
 from .models import Work
+from review.models import Review
+from django.contrib.auth import get_user_model
+import json
 
 class WorkTestCase(TestCase):
     def setUp(self):
-        Work.objects.create(title='DummyTitle', year=2019, description="HI", link="https://www.naver.com/",
-                            completion=True, platform_id=1,
-                            )
+        User = get_user_model()
+        author = User.objects.create_user(
+            email='dummy@user.com', password='1234', username='dummy')
+        author2 = User.objects.create_user(
+            email='dummy2@user.com', password='1234', username='dummy')
+        Work.objects.create(
+            title='DummyTitle', year=2019, description="HI", 
+            link="https://www.naver.com/",completion=True, platform_id=1
+        )
+        Review.objects.create(
+            work=Work.objects.first(), score=0.0, title="DUM", content="DUM_CONTENT", author=author
+        )
+        Review.objects.create(
+            work=Work.objects.first(), score=5.0, title="DUMMY2", content="DUMMY_CONTENT2", author=author2
+        )
+        
+
 
     def test_work_count(self):
         self.assertEqual(Work.objects.all().count(), 1)
@@ -13,15 +32,82 @@ class WorkTestCase(TestCase):
     def test_work_id(self):
         client = Client()
         response = client.get('/works/1/')
+        self.assertEqual(response.status_code, 200)
 
-        self.assertEqual(response.status_code, 501)
+        self.assertIn("DummyTitle", response.content.decode())
+        self.assertIn("2019", response.content.decode())
+        self.assertIn("HI", response.content.decode())
+        self.assertIn("https://www.naver.com/", response.content.decode())
 
-    def test_work_id_review(self):
+    def test_work_id_not_exist(self):
+        client = Client()
+        response = client.get('/works/2/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_article_wrong_api(self):
+        client = Client()
+        response = client.delete('/works/1/')
+
+        self.assertEqual(response.status_code, 405)
+        self.assertIn("GET", response.headers['Allow'])
+
+        response = client.post('/works/1/', {})
+
+        self.assertEqual(response.status_code, 405)
+        self.assertIn("GET", response.headers['Allow'])
+
+
+        response = client.put('/works/1/', {})
+
+        self.assertEqual(response.status_code, 405)
+        self.assertIn("GET", response.headers['Allow'])
+
+    def test_work_id_review_not_exist(self):
+        client = Client()
+        response = client.get('/works/2/reviews/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_work_id_review(self):
         client = Client()
         response = client.get('/works/1/reviews/')
 
-        self.assertEqual(response.status_code, 501)
+        self.assertIn('DUM', response.content.decode())
+        self.assertIn('DUM_CONTENT', response.content.decode())
+        self.assertIn('DUMMY2', response.content.decode())
+        self.assertIn('DUMMY_CONTENT2', response.content.decode())
+        self.assertIn('0.0', response.content.decode())
+        self.assertIn('5.0', response.content.decode())
 
+        self.assertEqual(2, len(json.loads(response.content.decode())))
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_work_id_review(self):     #TODO: change to login
+        client = Client()
+        #client.login(username = 'swpp', password = 'iluvswpp')
+        User = get_user_model()
+
+        """
+        author = User.objects.create_user(
+            email='dummy3@user.com', password='1234', username='dummy3'
+        )
+        """
+
+        review = Review(score=3.0, title="DUM", content="DUM_CONTENT")
+        review_json = model_to_dict(review)
+        response = client.post('/works/1/reviews/', review_json, content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+    
+    def test_post_work_id_review_not_logged_in(self):
+        client = Client()
+        
+        review = Review(score=3.0, title="DUM", content="DUM_CONTENT")
+        review_json = serializers.serialize('json', [review, ])
+        response = client.post('/works/1/reviews/', review_json[0], content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+    
     def test_work_main(self):
         client = Client()
         response = client.get('/works/main/')
--- backend/connectoon/work/views.py
+++ backend/connectoon/work/views.py
@@ -1,12 +1,60 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
+import json
+from django.http.response import HttpResponseBadRequest, JsonResponse
+from json.decoder import JSONDecodeError
 
+from work.models import Work
+from review.models import Review
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
 
 def work_id(request, id):  # TODO
-    return HttpResponse(status=501)
+    work = Work.objects.filter(id=id).values()
+    if not work:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        return JsonResponse(list(work), safe = False, status = 200)
+    else:
+        return HttpResponseNotAllowed(['GET'])
 
 
-def work_id_review(request, id):  # TODO
-    return HttpResponse(status=501)
+def work_id_review(request, id):
+    work = Work.objects.filter(id=id).values().first()
+    if not work:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        reviews = Review.objects.filter(work = id).values()
+        response_dict = []
+        
+        for review in reviews:
+            response_dict.append(review)
+        
+        return JsonResponse(response_dict, safe = False)
+
+
+    elif request.method == 'POST':
+        User = get_user_model()
+        user = User.objects.filter(id = request.user.id)
+        if not user:
+            return HttpResponse(status = 401)
+
+        try:
+            print(request.body.decode())
+            body = json.loads(request.body.decode())
+            title = body['title']
+            content = body['content']
+            score = body['score']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        
+        review = Review(user = user, work = work, title = title, content = content, score = score)
+        review.save()
+        return JsonResponse(review.value(), status = 201)
+    
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
 
 
 def work_main(request):  # TODO
