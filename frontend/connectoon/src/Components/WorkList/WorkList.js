48a84868516173c3cb6829906abf6751e290f909
kskim0119 <dooly9931@snu.ac.kr>
2021-11-06 23:24:11 +0900

    fix .css of WorkObject and WorkList, implement more button of main

--- frontend/connectoon/src/Components/WorkList/WorkList.js
+++ frontend/connectoon/src/Components/WorkList/WorkList.js
@@ -3,42 +3,56 @@ import PropTypes from 'prop-types';
 import WorkObject from '../WorkObject/WorkObject';
 import './WorkList.css';
 
-const WorkList = (props) => {
-  const {
-    workList, workDisplayNum, className, subject,
-  } = props;
-  const workObjects = workList.map((work) => (
-    <WorkObject
-      key={work.id}
-      className="work-object"
-      src={work.src}
-      platform={work.platform}
-      completion={work.completion}
-      title={work.title}
-      artist={work.artist}
-      createdYear={work.createdYear}
-      score={work.score}
-    />
-  ));
-  const displayingObjects = workObjects.slice(0, workDisplayNum);
+class WorkList extends Component {
+  constructor(props) {
+    super(props);
+    const { workList } = props;
+    const workObjects = workList.map((work) => (
+      <WorkObject
+        key={work.id}
+        className="work-object"
+        src={work.src}
+        platform={work.platform}
+        completion={work.completion}
+        title={work.title}
+        artist={work.artist}
+        createdYear={work.createdYear}
+        score={work.score}
+      />
+    ));
+    this.state = { totalDisplayRow: 1, workObjects };
+  }
 
-  const onClickMore = () => {
+  onClickMore = () => {
+    const { totalDisplayRow } = this.state;
+    this.setState({ totalDisplayRow: totalDisplayRow + 2 });
   };
 
-  return (
-    <div className={className}>
-      <div className="list-wrapper">
-        <h1 className="list-subject">{subject}</h1>
-        {displayingObjects}
-        {(workObjects.length > workDisplayNum) && <button type="button" className="more-works-button">more...</button>}
+  render() {
+    const {
+      workNumInRow, className, subject,
+    } = this.props;
+    const { totalDisplayRow, workObjects } = this.state;
+    let i = 0;
+    const displayingObjects = Array.from({ length: totalDisplayRow });
+    for (i = 0; i < totalDisplayRow; i += 1) {
+      displayingObjects[i] = workObjects.slice(i * workNumInRow, (i + 1) * workNumInRow);
+    }
+    return (
+      <div className={className}>
+        <div className="list-wrapper" align="left">
+          <h1 className="list-subject">{subject}</h1>
+          {displayingObjects}
+          {(workObjects.length > totalDisplayRow * workNumInRow) && <button type="button" className="more-works-button" onClick={() => this.onClickMore()}>more...</button>}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 WorkList.propTypes = {
   workList: PropTypes.arrayOf(PropTypes.object).isRequired,
-  workDisplayNum: PropTypes.number.isRequired,
+  workNumInRow: PropTypes.number.isRequired,
   className: PropTypes.string.isRequired,
   subject: PropTypes.string.isRequired,
 };
--- frontend/connectoon/src/Containers/Main/Main.js
+++ frontend/connectoon/src/Containers/Main/Main.js
@@ -52,6 +52,56 @@ const dummyWorks = [
     createdYear: '2019',
     score: '4.9',
   },
+  {
+    id: 6,
+    src: 'https://shared-comic.pstatic.net/thumb/webtoon/20853/thumbnail/thumbnail_IMAG06_89061d8c-e491-42f1-8c15-40932e5eb939.jpg',
+    platform: '/images/naver_logo.png',
+    completion: true,
+    title: '마음의 소리',
+    artist: '조석',
+    createdYear: '2006',
+    score: '4.7',
+  },
+  {
+    id: 7,
+    src: 'https://shared-comic.pstatic.net/thumb/webtoon/721948/thumbnail/thumbnail_IMAG06_eef5b6c4-39dc-46d9-89d1-1a1ee357b696.jpg',
+    platform: '/images/naver_logo.png',
+    completion: false,
+    title: 'Study Group',
+    artist: 'Shin, Hyeongwook & Yu, Seungyeon',
+    createdYear: '2019',
+    score: '4.9',
+  },
+  {
+    id: 8,
+    src: 'https://shared-comic.pstatic.net/thumb/webtoon/20853/thumbnail/thumbnail_IMAG06_89061d8c-e491-42f1-8c15-40932e5eb939.jpg',
+    platform: '/images/naver_logo.png',
+    completion: true,
+    title: '마음의 소리',
+    artist: '조석',
+    createdYear: '2006',
+    score: '4.7',
+  },
+  {
+    id: 9,
+    src: 'https://shared-comic.pstatic.net/thumb/webtoon/721948/thumbnail/thumbnail_IMAG06_eef5b6c4-39dc-46d9-89d1-1a1ee357b696.jpg',
+    platform: '/images/naver_logo.png',
+    completion: false,
+    title: 'Study Group',
+    artist: 'Shin, Hyeongwook & Yu, Seungyeon',
+    createdYear: '2019',
+    score: '4.9',
+  },
+  {
+    id: 10,
+    src: 'https://shared-comic.pstatic.net/thumb/webtoon/20853/thumbnail/thumbnail_IMAG06_89061d8c-e491-42f1-8c15-40932e5eb939.jpg',
+    platform: '/images/naver_logo.png',
+    completion: true,
+    title: '마음의 소리',
+    artist: '조석',
+    createdYear: '2006',
+    score: '4.7',
+  },
 ];
 class Main extends Component {
   constructor(props) {
@@ -63,8 +113,8 @@ class Main extends Component {
     const { displayNum } = this.state;
     return (
       <div className="main-page">
-        <WorkList className="most-reviewed-work-list" subject="Most revied works" workList={dummyWorks} workDisplayNum={displayNum} />
-        <WorkList className="most-liked-work-list" subject="Most liked works" workList={[...dummyWorks].reverse()} workDisplayNum={displayNum} />
+        <WorkList className="most-reviewed-work-list" subject="Most revied works" workList={dummyWorks} workNumInRow={displayNum} />
+        <WorkList className="most-liked-work-list" subject="Most liked works" workList={[...dummyWorks].reverse()} workNumInRow={displayNum} />
       </div>
     );
   }
