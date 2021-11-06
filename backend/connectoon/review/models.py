ab4d0c77e7f5c7a9c4aaa1b8399a90fc3ce4a55c
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-11-06 23:06:45 +0900

    modified review model

--- backend/connectoon/review/models.py
+++ backend/connectoon/review/models.py
@@ -3,10 +3,11 @@ from django.db.models.deletion import CASCADE
 from django.db.models.fields import DateTimeField
 
 from work.models import Work
+from django.contrib.auth import get_user_model
 
 class Review(models.Model):
-  work_id = models.ForeignKey(Work, on_delete=models.CASCADE)
-  #author_id = models.ForeignKey(, on_delete=models.CASCADE)
+  work = models.ForeignKey(Work, on_delete=models.CASCADE, related_name="work")
+  author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="author")
   score = models.FloatField()        #decimal?
   likes = models.IntegerField(default=0)
   title = models.CharField(max_length=200)
--- backend/connectoon/review/tests.py
+++ backend/connectoon/review/tests.py
@@ -1,14 +1,17 @@
 from django.test import TestCase, Client
 from work.models import Work
 from .models import Review
+from django.contrib.auth import get_user_model
 
 
 class ReviewTestCase(TestCase):
   def setUp(self):
+    User = get_user_model()
+    author = User.objects.create_user(email='dummy@user.com', password='1234', username='dummy')
     Work.objects.create(title = 'DummyTitle', year=2019, description="HI", link="https://www.naver.com/",
-      completion=True, platform_id=1,
+      completion=True, platform_id=1
     )
-    Review.objects.create(work_id = Work.objects.first(), score = 0.0, title = "DUM", content="DUM_CONTENT", updated_at ="")
+    Review.objects.create(work = Work.objects.first(), score = 0.0, title = "DUM", content="DUM_CONTENT", author = author)
   
   def test_review_count(self):
     self.assertEqual(Review.objects.all().count(), 1)
