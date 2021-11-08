633d85cb2ec127f984e13e7bce7b1bf8180cd478
fjvxmap <gratingtrumpet@gmail.com>
2021-11-08 15:40:24 +0900

    Implemented TagSearchWindow

--- frontend/connectoon/src/Containers/TagSearchWindow/TagSearchWindow.js
+++ frontend/connectoon/src/Containers/TagSearchWindow/TagSearchWindow.js
@@ -8,59 +8,34 @@ class TagSearchWindow extends Component {
     super(props);
     this.state = {
       genre: '',
-      dummyTagList: [
-        {
-          key: 1,
-          name: '판타지',
-          related: [2, 3],
-        },
-        {
-          key: 2,
-          name: '마법',
-          related: [1],
-        },
-        {
-          key: 3,
-          name: '구해주세요여긴날씨가나빠요물이점점불어나요누구보다나를먼저건져내줘요그대의버릇과습관따위가',
-          related: [],
-        },
-        {
-          key: 4,
-          name: '아시발꿈',
-          related: [],
-        },
-      ],
-      dummySelectedList: [],
     };
   }
 
-  onAddTag = (name) => {
-    const { dummySelectedList, dummyTagList } = this.state;
-    const selectedTag = dummyTagList.filter((x) => x.name === name)[0];
-    const updatedTagList = dummyTagList.filter((x) => x.name !== name);
-    this.setState({ dummyTagList: updatedTagList, dummySelectedList: [...dummySelectedList, selectedTag] });
+  onClickAddTag = (name) => {
+    const { onAddTag } = this.props;
+    onAddTag(name);
   }
 
-  onDeleteTag = (name) => {
-    const { dummySelectedList, dummyTagList } = this.state;
-    const selectedTag = dummySelectedList.filter((x) => x.name === name)[0];
-    const updatedSelectedList = dummySelectedList.filter((x) => x.name !== name);
-    this.setState({ dummyTagList: [...dummyTagList, selectedTag], dummySelectedList: updatedSelectedList });
+  onClickDeleteTag = (name) => {
+    const { onDeleteTag } = this.props;
+    onDeleteTag(name);
   }
 
   render() {
     const {
       className,
+      tags,
+      selectedTags,
     } = this.props;
-    const { genre, dummyTagList, dummySelectedList } = this.state;
+    const { genre } = this.state;
 
-    const foundList = dummyTagList.filter((x) => {
+    const foundList = tags.filter((x) => {
       let boolVal = false;
       if (genre.length === 0) {
-        if (dummySelectedList.length === 0) {
+        if (selectedTags.length === 0) {
           boolVal = true;
         } else {
-          boolVal = dummySelectedList[dummySelectedList.length - 1].related.includes(x.key);
+          boolVal = selectedTags[selectedTags.length - 1].related.includes(x.key);
         }
       } else {
         boolVal = x.name.indexOf(genre) !== -1;
@@ -71,18 +46,18 @@ class TagSearchWindow extends Component {
         tagName={x.name}
         deletable={false}
         selected={false}
-        onAddTag={this.onAddTag}
-        onDeleteTag={this.onDeleteTag}
+        onAddTag={this.onClickAddTag}
+        onDeleteTag={this.onClickDeleteTag}
       />
     ));
 
-    const clickedList = dummySelectedList.map((x) => (
+    const clickedList = selectedTags.map((x) => (
       <GenreTag
         tagName={x.name}
         deletable={true}
         selected={true}
-        onAddTag={this.onAddTag}
-        onDeleteTag={this.onDeleteTag}
+        onAddTag={this.onClickAddTag}
+        onDeleteTag={this.onClickDeleteTag}
       />
     ));
 
@@ -92,7 +67,7 @@ class TagSearchWindow extends Component {
           <label id="search-genre-tag">Genre</label>
           <input type="text" id="tag-search-input" value={genre} onChange={(e) => { this.setState({ genre: e.target.value }); }} />
         </div>
-        {dummySelectedList.length !== 0 && <div className="selected-tag">
+        {selectedTags.length !== 0 && <div className="selected-tag">
           {clickedList}
         </div>}
         <div className="found-list">
@@ -105,6 +80,10 @@ class TagSearchWindow extends Component {
 
 TagSearchWindow.propTypes = {
   className: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.any).isRequired,
+  selectedTags: PropTypes.arrayOf(PropTypes.any).isRequired,
+  onAddTag: PropTypes.func.isRequired,
+  onDeleteTag: PropTypes.func.isRequired,
 };
 
 export default TagSearchWindow;
