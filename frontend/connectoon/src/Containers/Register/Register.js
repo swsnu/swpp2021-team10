4d37aadb0ab9148f232f14d216205a1d3cf795b8
veldic <rsual@snu.ac.kr>
2021-11-25 17:18:20 +0900

    Change className to id

--- frontend/connectoon/src/Containers/Register/Register.js
+++ frontend/connectoon/src/Containers/Register/Register.js
@@ -80,41 +80,41 @@ class Register extends Component {
     }
 
     return (
-      <div className="register-page">
+      <div id="register-page">
         <div id="profile-image-holder">
-          {this.state.selectedImage && <img className="register-profile-img" width="250px" src={URL.createObjectURL(this.state.selectedImage)} />}
+          {this.state.selectedImage && <img id="register-profile-img" width="250px" src={URL.createObjectURL(this.state.selectedImage)} />}
         </div>
         <input type="file" id="input-profile-image" accept=".jpg, .png, .jpeg" name="profile-image" onChange={(event) => { this.setState({ selectedImage: event.target.files[0] }); }} />
         <label id="upload-image-label" htmlFor="input-profile-image">
           Upload Profile Image
         </label>
-        <label className="register-email">
+        <label id="register-email">
           <div id="email-label-text">email</div>
-          <input className="register-email-input" type="text" value={this.state.email} onChange={(event) => { this.setState({ email: event.target.value, emailDupCheck: false }); }} />
-          <button className="register-dupchk-email-button" onClick={() => this.checkEmailDup()} type="button">check email duplication</button>
-          { this.state.emailDupCheck && <img className="register-dupchk-email-img" src="/images/check.png" alt="dupchk-email" width="20px" /> }
+          <input id="register-email-input" type="text" value={this.state.email} onChange={(event) => { this.setState({ email: event.target.value, emailDupCheck: false }); }} />
+          <button id="register-dupchk-email-button" onClick={() => this.checkEmailDup()} type="button">check email duplication</button>
+          { this.state.emailDupCheck && <img id="register-dupchk-email-img" src="/images/check.png" alt="dupchk-email" width="20px" /> }
         </label>
-        <label className="register-username">
+        <label id="register-username">
           <div id="username-label-text">username</div>
-          <input className="register-username-input" type="text" value={this.state.username} onChange={(event) => { this.setState({ username: event.target.value, usernameDupCheck: false }); }} />
-          <button className="register-dupchk-username-button" onClick={() => this.checkUsernameDup()} type="button">check username duplication</button>
-          { this.state.usernameDupCheck && <img className="register-dupchk-username-img" src="/images/check.png" alt="dupchk-username" width="20px" /> }
+          <input id="register-username-input" type="text" value={this.state.username} onChange={(event) => { this.setState({ username: event.target.value, usernameDupCheck: false }); }} />
+          <button id="register-dupchk-username-button" onClick={() => this.checkUsernameDup()} type="button">check username duplication</button>
+          { this.state.usernameDupCheck && <img id="register-dupchk-username-img" src="/images/check.png" alt="dupchk-username" width="20px" /> }
         </label>
-        <label className="register-password">
+        <label id="register-password">
           <div id="password-label-text">password</div>
-          <input className="register-password-input" type="password" value={this.state.password} onChange={(event) => { this.setState({ password: event.target.value }); }} />
+          <input id="register-password-input" type="password" value={this.state.password} onChange={(event) => { this.setState({ password: event.target.value }); }} />
         </label>
-        <label className="register-password-check">
+        <label id="register-password-check">
           <div id="password-check-label-text">password check</div>
-          <input className="register-password-check-input" type="password" value={this.state.passwordCheck} onChange={(event) => { this.setState({ passwordCheck: event.target.value }); }} />
-          { this.checkPasswordAccord() && <img className="register-password-accord-img" src="/images/check.png" alt="password-accord" /> }
+          <input id="register-password-check-input" type="password" value={this.state.passwordCheck} onChange={(event) => { this.setState({ passwordCheck: event.target.value }); }} />
+          { this.checkPasswordAccord() && <img id="register-password-accord-img" src="/images/check.png" alt="password-accord" /> }
         </label>
-        <h4 className="register-favorite-genres">
+        <h4 id="register-favorite-genres">
           Favorite Genres
         </h4>
-        {/* <TagSearchWindow className="search-genre-search-window" onAddTag={this.onAddTag} onDeleteTag={this.onDeleteTag} /> */}
+        {/* <TagSearchWindow id="search-genre-search-window" onAddTag={this.onAddTag} onDeleteTag={this.onDeleteTag} /> */}
         <br />
-        <button className="register-submit-button" onClick={() => this.submitRegisterData()} type="button" disabled={!(this.state.emailDupCheck && this.state.usernameDupCheck && this.checkPasswordAccord())}>submit</button>
+        <button id="register-submit-button" onClick={() => this.submitRegisterData()} type="button" disabled={!(this.state.emailDupCheck && this.state.usernameDupCheck && this.checkPasswordAccord())}>submit</button>
       </div>
     );
   }
