b42e0cbf5027da0ab3f0618b04c4144b9cfd801b
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-12-01 03:39:01 +0900

    frontend testing for like

--- frontend/connectoon/src/store/reducers/review.test.js
+++ frontend/connectoon/src/store/reducers/review.test.js
@@ -8,6 +8,9 @@ const stubReview2 = { id: 2, title: 'test', content: 'test' };
 const stubInitialState = {
   reviews: [stubReview1, stubReview2],
 };
+const stubReview = {
+  id: 1, title: 'test', content: 'test', clickedLike: false,
+};
 
 describe('Review Reducer', () => {
   it('should return default state', () => {
@@ -45,7 +48,57 @@ describe('Review Reducer', () => {
       reviews: stubReviews,
     });
     expect(newState).toEqual({
+<<<<<<< HEAD
       reviews: stubReviews,
+=======
+      reviews: [],
+      selectedReview: stubSelectedReview,
+      boardReviews: [],
     });
+  });
+
+  it('should get reviews for board', () => {
+    const stubReview2 = { id: 2, title: 'test', content: 'test' };
+    const stubBoardReviews = [stubReview, stubReview];
+    const newState = reducer(undefined, {
+      type: actionTypes.GET_BOARD_REVIEWS,
+      boardReviews: stubBoardReviews,
+    });
+    expect(newState).toEqual({
+      reviews: [],
+      selectedReview: null,
+      boardReviews: stubBoardReviews,
+>>>>>>> 056d6a1 (frontend testing for like)
+    });
+  });
+
+  it('should post like', () => {
+    const stubReview2 = {
+      id: 2, title: 'test2', content: 'test2', clickedLike: false,
+    };
+    const stubInitialState = {
+      reviews: [stubReview, stubReview2],
+      selectedReview: null,
+    };
+    const newState = reducer(stubInitialState, {
+      type: actionTypes.POST_LIKE,
+      likeTargetReview: stubReview2,
+    });
+    expect(newState.reviews[1].clickedLike).toBe(false);
+  });
+
+  it('should post unlike', () => {
+    const stubReview2 = {
+      id: 2, title: 'test2', content: 'test2', clickedLike: true,
+    };
+    const stubInitialState = {
+      reviews: [stubReview, stubReview2],
+      selectedReview: null,
+    };
+    const newState = reducer(stubInitialState, {
+      type: actionTypes.POST_UNLIKE,
+      unlikeTargetReview: stubReview2,
+    });
+    expect(newState.reviews[1].clickedLike).toBe(true);
   });
 });
