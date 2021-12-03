1968aa24236e5ea59368d2f8b3d6c9203c8b1943
dooly9931 <48858912+dooly9931@users.noreply.github.com>
2021-12-03 21:35:47 +0900

    Scroll을 위한 state 제거, 없는 작품 url 접근 시 예외처리 (#60)
    * add NoSuchWork Case at WorkDetail and corresponding action/reducer, remove reviewNum state from WorkDetail which was for scrolling
    
    * delete reviewNum state from render() of WorkDetail

--- frontend/connectoon/src/store/actions/actionTypes.js
+++ frontend/connectoon/src/store/actions/actionTypes.js
@@ -13,6 +13,7 @@ export const EDIT_MYUSER = 'EDIT_MYUSER';
 export const GET_MYREVIEWS = 'GET_MYREVIEWS';
 export const GET_MAIN_WORKS = 'GET_MAIN_WORKS';
 export const GET_WORK = 'GET_WORK';
+export const WORK_NOT_EXISTING = 'WORK_NOT_EXISTING';
 export const GET_WORK_REVIEWS = 'GET_WORK_REVIEWS';
 export const GET_REC_WORKS = 'GET_REC_WORKS';
 export const GET_SEARCH_WORKS = 'GET_SEARCH_WORKS';
