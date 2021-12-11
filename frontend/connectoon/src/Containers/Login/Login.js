ddfa97478f3c3bc727edde493eae4f744ac10b82
SeungMin Lee <rsual@snu.ac.kr>
2021-12-11 18:40:14 +0900

    Add css to Login Page (#72)
    * Add css to Login Page
    
    * Modify Login CSS

--- frontend/connectoon/src/Containers/Login/Login.js
+++ frontend/connectoon/src/Containers/Login/Login.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router';
 
+import './Login.css';
+
 import * as actionCreators from '../../store/actions/index';
 
 class Login extends Component {
@@ -28,16 +30,21 @@ class Login extends Component {
 
     return (
       <div className="login-page">
-        <label id="login-email">
-          <div id="email-label-text">email</div>
-          <input id="login-email-input" type="email" value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
-        </label>
-        <label id="login-password">
-          <div id="password-label-text">password</div>
-          <input id="login-password-input" type="password" value={this.state.password} onChange={(event) => this.setState({ password: event.target.value })} />
-        </label>
-        <button id="login-submit-button" type="button" onClick={() => this.onClickSubmit()}>submit</button>
-        <Link id="login-register-link" to="/register">register</Link>
+        <div className="login-page-container">
+          <label id="login-email">
+            <div id="email-label-text">email</div>
+            <input id="login-email-input" type="email" value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
+          </label>
+          <label id="login-password">
+            <div id="password-label-text">password</div>
+            <input id="login-password-input" type="password" value={this.state.password} onChange={(event) => this.setState({ password: event.target.value })} />
+          </label>
+          <div className="login-button-div">
+            <button id="login-submit-button" type="button" onClick={() => this.onClickSubmit()}>submit</button>
+            <br />
+            <Link id="login-register-link" to="/register">register</Link>
+          </div>
+        </div>
       </div>
     );
   }
