b946bdae3df8bc1900a1923ff7ea6fbf231f55e4
SeungMin Lee <rsual@snu.ac.kr>
2021-12-16 13:25:48 +0900

    Add User Id Feature (#95)

--- backend/connectoon/user/tests.py
+++ backend/connectoon/user/tests.py
@@ -40,6 +40,8 @@ class UserTestCase(TestCase):
         tag1 = Tag.objects.create(name='tag1')
         tag2 = Tag.objects.create(name='tag2')
         tag3 = Tag.objects.create(name='tag3')
+        tag1.related.add(tag3)
+        tag1.save()
 
         user1 = user_class.objects.create_user(email='test1@snu.ac.kr', username='test1', password='qwe123')
 
@@ -223,8 +225,10 @@ class UserTestCase(TestCase):
     def test_user_id(self):
         client = Client()
         response = client.get('/api/v1/users/1/')
+        self.assertEqual(response.status_code, 200)
 
-        self.assertEqual(response.status_code, 501)
+        response = client.get('/api/v1/users/100/')
+        self.assertEqual(response.status_code, 400)
 
     def test_user_me_get(self):
         client = Client(enforce_csrf_checks=True)
