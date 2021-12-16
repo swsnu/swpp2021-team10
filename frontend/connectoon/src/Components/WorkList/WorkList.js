43565432903993c601f3c138debe2a00edd32e25
hursuhyun <hursuhyun@csap.snu.ac.kr>
2021-12-16 17:41:11 +0900

    M

--- frontend/connectoon/src/Components/WorkList/WorkList.js
+++ frontend/connectoon/src/Components/WorkList/WorkList.js
@@ -25,46 +25,10 @@ const WorkList = (props) => {
     />
   ));
 
-<<<<<<< HEAD
   let i = 0;
   const displayingObjects = Array.from({ length: rows });
   for (i = 0; i < rows; i += 1) {
     displayingObjects[i] = workObjects.slice(worksInRow * i, worksInRow * (i + 1));
-=======
-  render() {
-    const {
-      workList, workNumInRow, className, subject,
-    } = this.props;
-    const { totalDisplayRow } = this.state;
-    const workObjects = workList?.map((work) => (
-      <WorkObject
-        key={String(work.id) + work.title}
-        className="work-object"
-        src={work.thumbnail_picture}
-        platform={platformMapper[work.platform_id]}
-        completion={work.completion}
-        title={work.title}
-        artists={work.artists}
-        createdYear={work.year}
-        score={work.score_avg}
-        onClickWork={() => this.onClickWorkObject(work.id)}
-      />
-    ));
-    let i = 0;
-    const displayingObjects = Array.from({ length: totalDisplayRow });
-    for (i = 0; i < totalDisplayRow; i += 1) {
-      displayingObjects[i] = workObjects.slice(i * workNumInRow, (i + 1) * workNumInRow);
-    }
-    return (
-      <div className={className}>
-        <div className="list-wrapper" align="left">
-          <h1 className="list-subject">{subject}</h1>
-          {displayingObjects}
-          {(workObjects.length > totalDisplayRow * workNumInRow) && <button type="button" className="more-works-button" onClick={() => this.onClickMore()}>more...</button>}
-        </div>
-      </div>
-    );
->>>>>>> 9d67362 (fixing)
   }
 
   return (
